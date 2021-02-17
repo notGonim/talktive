9faf728bd3d5be23b93b7621ca89e22002ee3835
notGonim <mhmodgonim96@gmail.com>
2021-02-17 17:53:50 +0200

    Start with main home page

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAxsxnYw884Xwy52GK8bK0GtA3Jgcq2A9c",
+    authDomain: "talktive-6c393.firebaseapp.com",
+    projectId: "talktive-6c393",
+    storageBucket: "talktive-6c393.appspot.com",
+    messagingSenderId: "71011831049",
+    appId: "1:71011831049:web:f66e45d099cade118517d8"
+  };
---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './App.css';
+
+export const Home = () => {
+    return (
+        <div className="App">
+
+
+
+            <div className="app__body">
+
+
+                
+            </div>
+        </div>
+    )
+}
