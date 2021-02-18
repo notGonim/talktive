d0f55d52761444ac069b8bb169d145e2b9bf76a7
notGonim <mhmodgonim96@gmail.com>
2021-02-18 19:05:41 +0200

    Added the style to chat component

---
+++ src/components/sidebar/chat/Chat.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './chat.scss'
+
+export const Chat = () => {
+    return (
+        <div className="chatContainer">
+
+
+
+        </div>
+    )
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Chat } from '../components/sidebar/chat/Chat'
 import { Sidebar } from '../components/sidebar/Sidebar'
 import '../styles/home.scss'
 export const Home = () => {
     return (
         <div className="app">
             <div className="app__body">
-                <Sidebar/>
-
+                <Sidebar />
+                <Chat />
 
             </div>
         </div>
