325b59b297baaec702753d8e5bbcc0c184e03523
notGonim <mhmodgonim96@gmail.com>
2021-02-18 19:08:26 +0200

    Some Config at the strucutre of the code

--- src/components/sidebar/chat/Chat.js
+++ src/components/sidebar/chat/Chat.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import { Avatar } from '@material-ui/core'
+import React, { useState } from 'react'
 import './chat.scss'
 
 export const Chat = () => {
+
+    const [seeds, setSeeds] = useState('')
+
     return (
         <div className="chatContainer">
+            <div className="chat__header">
+                <Avatar />
+            </div>
+            <div className="chat__body">
 
+            </div>
+            <div className="chat__footer">
 
-
+            </div>
         </div>
     )
 }
