94ee6e978d6f3f86420d6f2813fb2e07e0fec8d6
notGonim <mhmodgonim96@gmail.com>
2021-02-19 10:31:37 +0200

    Start with the chat container footer

--- src/components/sidebar/chat/Chat.js
+++ src/components/sidebar/chat/Chat.js
@@ -1,26 +1,55 @@
-import { Avatar } from '@material-ui/core'
-import React, { useState } from 'react'
+import { Avatar, IconButton } from '@material-ui/core'
+import React, { useState, useEffect } from 'react'
 import './chat.scss'
+import SearchIcon from '@material-ui/icons/Search';
+import AttachFileIcon from '@material-ui/icons/AttachFile';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+import MicIcon from '@material-ui/icons/Mic';
 
 export const Chat = () => {
 
     const [seeds, setSeeds] = useState('')
 
+    useEffect(() => {
+        (Math.floor(Math.random() * 5000))
+    }, [])
     return (
         <div className="chatContainer">
             <div className="chat__header">
-                <Avatar />
+                <Avatar src={`https://avatars.dicebear.com/api/human/:${seeds}.svg`} />
                 <div className="chat__headerInfo">
                     <h3>Room name</h3>
                     <p>Last seen at ...</p>
                 </div>
-
+                <div className="chat__chatHeaderRight">
+                    <IconButton>
+                        <SearchIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
             <div className="chat__body">
-
+                <p className={`chat__message ${true && ' chat__reciver'}`}>
+                    <span className="chat__name">XXXX</span>
+                hey guys
+            <span className="chat__timestamp">
+                        3:52pm
+            </span>
+                </p>
             </div>
             <div className="chat__footer">
-
+                <InsertEmoticonIcon />
+                <form>
+                    <input type="text" />
+                    <button>Send a message</button>
+                </form>
+                <MicIcon />
             </div>
         </div>
     )
