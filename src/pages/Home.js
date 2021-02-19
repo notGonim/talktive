94511ef793521ea0e4cf31feff7ad8731dc4bf07
notGonim <mhmodgonim96@gmail.com>
2021-02-19 13:25:12 +0200

    ADD google auth

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import { Chat } from '../components/chat/Chat'
 import { Sidebar } from '../components/sidebar/Sidebar'
 import '../styles/home.scss'
 export const Home = () => {
+    const [user, setUser] = useState(null)
     return (
         <div className="app">
-            <div className="app__body">
-                <Router>
-                    <Sidebar />
-                    <Switch>
-                        <Route path='/rooms/:roomId'>
-                            <Chat />
-                        </Route>
-                        <Route path="/">
-                            <Chat />
-                        </Route>
-                    </Switch>
-                </Router>
 
-            </div>
+
+            {!user ?
+                (
+                    <h1>Login</h1>
+                )
+                :
+                (
+                    <div className="app__body">
+                        <Router>
+                            <Sidebar />
+                            <Switch>
+                                <Route path='/rooms/:roomId'>
+                                    <Chat />
+                                </Route>
+                                <Route path="/">
+                                    <Chat />
+                                </Route>
+                            </Switch>
+                        </Router>
+
+                    </div>
+                )}
         </div>
+
     )
 }
